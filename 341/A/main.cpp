5ffd5784598c7c33b272981f41f45c5d5ab44138
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-02-01 20:19:42 +0600

    odd, even number fact

---
+++ 341/A/main.cpp
@@ -0,0 +1,80 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+#include<climits>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+
+int main()
+{
+    int n;
+    
+    int i;
+    while(cin >> n){
+        ull sum = 0;
+        ull odd_min=0;
+        ull a;
+        vector<int> v;
+        forl(i,0,n){
+            cin >> a;
+            if(a&1){
+                v.pb(a);
+            }
+            else{
+                sum += a;
+            }
+        }
+        sort(all(v));
+        int sz = v.size();
+        for(i=sz-1;i>0;i-=2){
+            sum += v[i] + v[i-1];
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
