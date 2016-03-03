400361ce54c4b8ef168af239367b4b79b4a06d63
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-03-04 01:46:14 +0600

    bitwise or

---
+++ 344/A/main.cpp
@@ -0,0 +1,82 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+#include<climits>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 1005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int a[MAX];
+    int b[MAX];
+    int i,j,k,n;
+    
+    while(cin >> n){
+        forl(i,0,n){
+            cin >> a[i];
+        }
+        forl(i,0,n){
+            cin >> b[i];
+        }
+        int mx = 0;
+        forl(i,0,n){
+            int tmp1 = a[i];
+            int tmp2 = b[i];
+            mx = max(mx, tmp1 + tmp2);
+            forl(j,i+1,n){
+                tmp1 |= a[j];
+                tmp2 |= b[j];
+                mx = max(mx, tmp1 + tmp2);
+            }
+        }
+        cout << mx << endl;
+    }
+    return 0;
+}
