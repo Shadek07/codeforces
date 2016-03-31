e8d2e34e3627e42feb3c4c040bfec0aa8ca468f2
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-03-31 06:46:43 +0600

    simulation or math

---
+++ 346/A/main.cpp
@@ -0,0 +1,58 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int n,a,b;
+    while(cin >> n >> a >> b){
+        int pos = 1;
+        if(b<0)
+            pos = -1;
+        b = abs(b);
+        while(b){
+            if(pos==1){
+                a++;
+                if(a==n+1)
+                    a = 1;
+            }else{
+                a--;
+                if(a==0)
+                    a = n;
+            }
+            b--;
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
