7efd68a2f42a1085c4537ba4d04cfc36d39fbfa1
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-05-06 19:52:03 +0600

    geometry + sorting

---
+++ 349/B/main.cpp
@@ -0,0 +1,60 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int n;
+    vi v;
+    long long int i,a,b;
+    while(cin >> n){
+            v.clear();
+        forl(i,0,n){
+            cin >> a;
+            v.pb(a);
+        }
+        sort(all(v));
+        b=0;
+        a = v[n-1];
+        forl(i,0,n-1){
+            b += v[i];
+        }
+        if(a == b){
+            cout << 1 << endl;
+            continue;
+        }else{
+            cout << max(a,b)-min(a,b)+1 << endl;
+        }
+    }
+    return 0;
+}
