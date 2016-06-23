394686cb4a31de432339d904334f142a839c94d9
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-06-24 03:14:54 +0600

    idea of bubble sort

---
+++ 359/B/main.cpp
@@ -0,0 +1,54 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int arr[105];
+    int n,i,j,k;
+    while(cin >> n){
+        forl(i,0,n){
+            cin >> arr[i];
+        }
+        for(i=0;i<n;i++){
+            for(j=0;j<n-i-1;j++){
+                if(arr[j+1]<arr[j]){
+                    cout << j+1 << " " << j+2 << endl;
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
